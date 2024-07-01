f87b0025baff5a30abdf47c1d020c029150c6862
giovanaprosiak <giovana.8923@aluno.pr.senac.br>
2024-07-01 17:00:14 -0300

    aula 33
    pagina projetos

--- js/projetos.js
+++ js/projetos.js
@@ -1,38 +1,38 @@
 let divs = [];
 let titulos = ['LANCHE', 'PORCAO', 'ACAI', 'SORVETE', 'BEBIDA', 'SOBREMESA'];
 let textos = ['LANCHE 123', 'PORCAO 123', 'ACAI 123', 'SORVETE 123', 'BEBIDA 123', 'SOBREMESA 123'];
-let imgs = ['mulher empresaria.jpg','mulher empresaria.jpg','mulher empresaria.jpg','mulher empresaria.jpg','mulher empresaria.jpg','mulher empresaria.jpg'];
+let imgs = ['mulher empresaria.jpg', 'mulher empresaria.jpg', 'mulher empresaria.jpg', 'mulher empresaria.jpg', 'mulher empresaria.jpg', 'mulher empresaria.jpg'];
 
-var cards_projetos = document.getElementById('cards_projetos');
+// var cards_projetos = document.getElementById('cards_projetos');
 
-for (var i =0; i <titulos.length; i++){
-   var img = document.createElement('imag');
-   var h3 = document.createElement('h3');
-   var p = document.createElement('p');
+// for (var i =0; i <titulos.length; i++){
+//    var img = document.createElement('imag');
+//    var h3 = document.createElement('h3');
+//    var p = document.createElement('p');
 
-   img.src = '.../img' + imgs[i];
-   img.style.width = '5vw';
+//    img.src = '.../img' + imgs[i];
+//    img.style.width = '5vw';
 
-   h3.textContent = titulos[i];
+//    h3.textContent = titulos[i];
 
-   p.textContent = textos[i];
+//    p.textContent = textos[i];
 
-   divs[i].appendChild(img);
-   divs[i].appendChild(h3);
-   divs[i].appendChild(p);
+//    divs[i].appendChild(img);
+//    divs[i].appendChild(h3);
+//    divs[i].appendChild(p);
 
-}
+// }
 
 
 let bannerAtual = 1;
 const totalBanners = 3;
-let intervalodetroca;
+let intervaloTroca;
 
-function trocabanner (numero){
+function trocabanner(numero) {
    let banner = document.getElementById("banner");
-   banner.style.left = "-" + (numero1) + "00vw";
+   banner.style.left = "-" + (numero - 1) + "00vw";
 
-   for (let i = 1; i >= totalBanners; i++){
+   for(var i = 1; i<=totalBanners; i++){
       document.getElementById("btn" + i).style.width = "1.2vw";
       document.getElementById("btn" + i).style.height = "1.2vw";
    }
@@ -43,10 +43,20 @@ function trocabanner (numero){
 
 }
 
-function autoTrocaBanner (){
-   bannerAtual = (bannerAtual) >=(totalBanners) ? 1 : bannerAtual + 1;
-
+function startAutoSwap() {
+   intervaloTroca = setInterval(autoTrocaBanner, 3000); // Troca a cada 3 segundos
 }
+
+
+function autoTrocaBanner() {
+   bannerAtual = (bannerAtual) >= (totalBanners) ? 1 : bannerAtual + 1;
+   trocabanner(bannerAtual);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+   trocabanner(bannerAtual); // Inicializa o banner
+   startAutoSwap(); // Inicia a troca automática
+});
 
 
 
@@ -102,17 +112,17 @@ function autoTrocaBanner (){
 //let numerosPares = [];
 //let numeros = [5,2,7, 8, 9, 1, 3, 4, 6];
 //for(var i = 0; i < numeros.length ; i++){
-   // if(numeros [i]%2 === 0){
-   //     numerosPares.push(numeros[i]);
-   // }
+// if(numeros [i]%2 === 0){
+//     numerosPares.push(numeros[i]);
+// }
 //}
- // console.log (numerosPares);
+// console.log (numerosPares);
 
- // var frutas = ['maça', 'banana', 'pera', 'abacaxi'];
+// var frutas = ['maça', 'banana', 'pera', 'abacaxi'];
 //  var i = 0;
- // var contador;
- // while(frutas[i] != 'pera'){
-  // contador++
-  // i++;
-  //}
- // console.log('Antes de pera tem' +contador+ 'frutas');
+// var contador;
+// while(frutas[i] != 'pera'){
+// contador++
+// i++;
+//}
+// console.log('Antes de pera tem' +contador+ 'frutas');
